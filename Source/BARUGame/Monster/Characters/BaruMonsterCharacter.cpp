db7981acf1f28d836399f7faebd2a461aa1676ba
yuluck <abisdream76@gmail.com>
2026-09-07 20:00:41 +0900

    [feat] 몬스터 죽음 관련 작업중

--- Source/BARUGame/Monster/Characters/BaruMonsterCharacter.cpp
+++ Source/BARUGame/Monster/Characters/BaruMonsterCharacter.cpp
@@ -10,6 +10,7 @@
 #include "AbilitySystem/Attributes/BaruMonsterAttributeSet.h"
 
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Net/UnrealNetwork.h"
 #include "BaruLog.h"
 
 
@@ -44,6 +45,20 @@ ABaruMonsterCharacter::ABaruMonsterCharacter()
 	
 }
 
+void ABaruMonsterCharacter::GetLifetimeReplicatedProps(
+	TArray<FLifetimeProperty>& OutLifetimeProps
+) const
+{
+	// 부모 Character가 사용하는 복제 설정도 함께 등록
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// 서버의 bIsDead 값이 변경되면 클라이언트로 복제
+	// 클라이언트에서는 값이 도착한 뒤 OnRep_IsDead가 자동 호출됨
+	DOREPLIFETIME(
+		ABaruMonsterCharacter,
+		bIsDead
+	);
+}
 
 void ABaruMonsterCharacter::BeginPlay()
 {
@@ -228,4 +243,65 @@ void ABaruMonsterCharacter::GrantInitialAbilities()
 			1
 		)
 	);
+}
+
+//---------
+//Dead 관련
+//---------
+
+void ABaruMonsterCharacter::Die_Implementation(AActor* Killer)
+{
+	// 사망 판정은 서버에서만 처리하며 중복 실행을 막음
+	if (!HasAuthority() || bIsDead)
+	{
+		return;
+	}
+
+	bIsDead = true;
+
+	// 서버 화면에도 즉시 사망 상태를 적용
+	// 클라이언트에서는 bIsDead가 복제될 때 자동 호출됨
+	OnRep_IsDead();
+
+	BARU_NET_LOG(
+		this,
+		LogBaruCombat,
+		Log,
+		TEXT("Monster died. Killer=%s"),
+		*GetNameSafe(Killer)
+	);
+}
+
+bool ABaruMonsterCharacter::IsDead_Implementation() const
+{
+	return bIsDead;
+}
+
+void ABaruMonsterCharacter::OnRep_IsDead()
+{
+	if (!bIsDead)
+	{
+		return;
+	}
+
+	// 진행 중인 이동을 멈추고 이후 이동도 차단
+	if (UCharacterMovementComponent* MovementComponent =
+		GetCharacterMovement())
+	{
+		MovementComponent->StopMovementImmediately();
+		MovementComponent->DisableMovement();
+	}
+
+	// 실행 중인 공격 Ability를 모두 중단
+	if (IsValid(AbilitySystemComponent))
+	{
+		AbilitySystemComponent->CancelAllAbilities();
+	}
+
+	// AI가 요청한 이동도 중단
+	if (AController* MonsterController = GetController())
+	{
+		MonsterController->StopMovement();
+	}
+	
 }
--- Source/BARUGame/Monster/Characters/BaruMonsterCharacter.h
+++ Source/BARUGame/Monster/Characters/BaruMonsterCharacter.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "AbilitySystemInterface.h"
+#include "Interfaces/CombatInterface.h"
 #include "BaruMonsterCharacter.generated.h"
 
 
@@ -17,7 +18,10 @@ class UBaruMonsterAttributeSet;
 struct FOnAttributeChangeData;
 
 UCLASS()
-class BARUGAME_API ABaruMonsterCharacter : public ACharacter, public IAbilitySystemInterface
+class BARUGAME_API ABaruMonsterCharacter
+	: public ACharacter, 
+	  public IAbilitySystemInterface,
+	  public ICombatInterface
 {
 	GENERATED_BODY()
 
@@ -62,13 +66,30 @@ protected:
 	
 	// DataAsset에 지정된 몬스터 Ability를 서버에서 ASC에 등록
 	void GrantInitialAbilities();
+	
+	UPROPERTY(
+		ReplicatedUsing = OnRep_IsDead,
+		VisibleInstanceOnly,
+		BlueprintReadOnly,
+		Category = "Monster|Combat"
+	)
+	bool bIsDead = false;
+	
+	UFUNCTION()
+	void OnRep_IsDead();
 
 public:
 	
 	//이 몬스터가 사용하는 설정표를 반환
 	const UBaruMonsterDataAsset* GetMonsterDataAsset() const;
+	
 	// 이 몬스터가 사용하는 ASC를 공통 인터페이스를 통해 반환
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	
+	// 몬스터 사망 처리
+	virtual void Die_Implementation(AActor* Killer) override;
+
+	// 현재 사망 상태 반환
+	virtual bool IsDead_Implementation() const override;
 	
 };
