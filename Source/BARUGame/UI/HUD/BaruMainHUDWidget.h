087b7f42b30984676af417b9c3cfaacec909f9d7
taehyun-0423 <skc6744@gmail.com>
2026-09-07 19:54:58 +0900

    feat: connect main HUD player status

--- Source/BARUGame/UI/HUD/BaruMainHUDWidget.h
+++ Source/BARUGame/UI/HUD/BaruMainHUDWidget.h
@@ -7,6 +7,12 @@
 
 #include "BaruMainHUDWidget.generated.h"
 
+class ABaruPlayerState;
+class UBaruHealthComponent;
+class UProgressBar;
+class UTextBlock;
+class UBorder;
+
 /**
  * 플레이 중 항상 표시되는 Main HUD의 C++ 기반 클래스,
  *
@@ -46,7 +52,68 @@ protected:
 	 */
 	virtual void NativeOnDeactivated() override;
 	
-protected:
+	// 로컬 플레이어의 PlayerState와 HUD를 연결한다.
+	void BindToPlayerState();
+	
+	// 등록했던 체력, 정신력 델리게이트를 해제한다.
+	void UnbindFromPlayerState();
+	
+	// 현재 PlayerState 값을 HUD에 한 번에 표시한다.
+	void RefreshPlayerStatus();
+	
+	// 체력 ProgressBar와 숫자 Text를 갱신한다.
+	void UpdateHealthDisplay();
+	
+	// 정신력 ProgressBar와 숫자 Text를 갱신한다.
+	void UpdateSanityDisplay();
+	
+	UFUNCTION()
+	void HandleHealthChanged(
+		UBaruHealthComponent* HealthComponent,
+		float OldHealth,
+		float NewHealth,
+		AActor* Instigator);
+	
+	UFUNCTION()
+	void HandleMaxHealthChanged(
+		float OldMaxHealth,
+		float NewMaxHealth);
+	
+	UFUNCTION()
+	void HandleSanityChanged(float NewSanity);
+	
+	// 체력 게이지
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UProgressBar> ProgressBar_Health;
+	
+	// 현재 체력 / 최대 체력
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_HealthValue;
+	
+	// 정신력 게이지
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UProgressBar> ProgressBar_Sanity;
+	
+	// 현재 정신력
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_SanityValue;
+	
+	// 상호작용 안내 전체 영역
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UBorder> Border_InteractionPrompt;
+	
+	// 상호작용 안내 문구
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_InteractionPrompt;
+	
+	// 현재 HUD가 관찰하고 잇는 로컬 BoundPlayerState;
+	UPROPERTY(Transient)
+	TObjectPtr<ABaruPlayerState> BoundPlayerState;
+	
+	// 현재 HUD가 관찰하고 있는 체력 컴포넌트
+	UPROPERTY(Transient)
+	TObjectPtr<UBaruHealthComponent> BoundHealthComponent;
+	
 	// [08.30] 포커스 타깃을 nullptr로 돌려 CommonUI의 Slate 포커스 강탈 방지
 	virtual UWidget* NativeGetDesiredFocusTarget() const override { return nullptr; }
 };
